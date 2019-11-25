7beb31d761e4bf83dde8b62d7b5043045ffd92eb
DeveloperPrince <princekudzaimaposa94@gmail.com>
2019-11-25 11:52:58 +0200

    Reduced Loss on Model and Set Bokeh Framework

--- model/main.py
+++ model/main.py
@@ -1,7 +1,9 @@
-from __future__ import absolute_import, division, print_function
 
+from __future__ import absolute_import, division, print_function
+#from bokeh.plotting import figure, output_file, show
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import regularizers
 # import tensorflow as tf currently no support for tensorflow manually install(make sure pc  supports tensorlfow)
 import pandas as pd 
 import os
@@ -33,13 +35,21 @@ class MODEL():
         model.add(Dense(30, init='uniform', activation='sigmoid'))
         model.add(Dense(1, init='uniform', activation='sigmoid'))                                                                                                                                                                                                                       
         # Compile model
-        model.compile(loss='mean_squared_error', optimizer='adam',
+        model.compile(loss='mean_squared_logarithmic_error', optimizer='adam',
         metrics=['accuracy'])
-        #Model Summary
+       
+        # Model Summary
         model.summary()
         # Fit the model
-        model.fit(features_train, labels_train, epochs=10000, batch_size=batch_size, verbose=2)
-
+        model.fit(features_train, labels_train, epochs=100, batch_size=batch_size, verbose=2)
+        model_yaml = model.to_yaml()
+        with open("seq01_model.yaml", "w") as yaml_file:
+            yaml_file.write(model_yaml)
+        # serialize weights to HDF5
+        model_json = model.to_json()
+        with open("seq01_model.json", "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights("seq01_model.h5")
         score = model.evaluate(features_test, labels_test, verbose=1)
         # round predictions
         accuracy = score[1]
@@ -58,11 +68,19 @@ class MODEL():
         # Compile model
         model.compile(loss='mean_squared_error', optimizer='adam',
         metrics=['accuracy'])
+
         # Model Summary
         model.summary()
         # Fit the model
-        model.fit(features_train, labels_train, epochs=10000, batch_size=batch_size, verbose=2)
-
+        model.fit(features_train, labels_train, epochs=100, batch_size=batch_size, verbose=2)
+        model_yaml = model.to_yaml()
+        with open("seq02_model.yaml", "w") as yaml_file:
+            yaml_file.write(model_yaml)
+        # serialize weights to HDF5
+        model_json = model.to_json()
+        with open("seq02_model.json", "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights("seq02_model.h5")
         score = model.evaluate(features_test, labels_test, verbose=1)
         # round predictions
         accuracy = score[1]
@@ -73,29 +91,33 @@ class MODEL():
     def kr_train_DNN_Seq_03(self,x_dim ,features_train ,features_test, labels_train , labels_test, batch_size):
         # create model
         model = Sequential()
-        model.add(Dense(10, input_dim=x_dim, init='uniform', activation='relu'))
+        model.add(Dense(10, input_dim=x_dim, init='uniform',kernel_regularizer=regularizers.l2(0.01),activity_regularizer=regularizers.l2(0.005), activation='relu'))
+        model.add(Dense(20, init='uniform',kernel_regularizer=regularizers.l2(0.015),activity_regularizer=regularizers.l1(0.02), activation='relu'))
         model.add(Dense(20, init='uniform', activation='relu'))
-        model.add(Dense(20, init='uniform', activation='sigmoid'))
-        model.add(Dense(1, init='uniform', activation='sigmoid'))                                                                                                                                                                                                                       
+        model.add(Dense(1, init='uniform', activation='relu'))                                                                                                                                                                                                                
         # Compile model
         model.compile(loss='mean_squared_logarithmic_error', optimizer='adam',
         metrics=['accuracy'])
         # Model Summary
         model.summary()
         # Fit the model
-        model.fit(features_train, labels_train, epochs=100, batch_size=batch_size, verbose=2)
+        history = model.fit(features_train, labels_train, epochs=100, batch_size=batch_size, verbose=2)
         model_yaml = model.to_yaml()
-        with open("seq_model.yaml", "w") as yaml_file:
+        with open("seq03_model.yaml", "w") as yaml_file:
             yaml_file.write(model_yaml)
         # serialize weights to HDF5
         model_json = model.to_json()
-        with open("seq_model.json", "w") as json_file:
+        with open("seq03_model.json", "w") as json_file:
             json_file.write(model_json)
-        model.save_weights("seq_model.h5")
+        model.save_weights("seq03_model.h5")
         score = model.evaluate(features_test, labels_test, verbose=1)
         # round predictions
         accuracy = score[1]
         accuracy = accuracy * 100
-    
+        #output_file("line_plot.html")
+        #line_graph = figure()
+        #line_graph.line([history.history['acc']], [history.history['val_acc']])
+        #show(line_graph)
+	
         return accuracy
  
